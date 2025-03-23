469fb31d5f41627b537bcce5ba0890cd2394022a
Jing <jingli0319@gmail.com>
2025-03-23 20:41:46 +0100

    add random color to other player's images

--- app/components/ladder.tsx
+++ app/components/ladder.tsx
@@ -1,3 +1,4 @@
+import { useRef, useEffect } from "react";
 import { teams } from "../data/teams";
 
 // Props für die Ladder-Komponente
@@ -44,11 +45,58 @@ const Ladder: React.FC<LadderProps> = ({
     }
   };
 
+  // Logik für die random Farbe des Playerfotos
+  const getRandomColor = () => {
+    // return "#" + Math.floor(Math.random() * 16777215).toString(16);
+    const colors = [
+      "#86B084", // Original color
+      "#7DA3B9", // Blue-gray
+      "#B08679", // Dusty rose
+      "#A186B0", // Lavender
+      "#B0A186", // Tan
+      "#79B086", // Seafoam
+      "#86B079", // Sage
+      "#B079A1", // Mauve
+      "#79A1B0", // Steel blue
+      "#A1B079", // Olive
+      "#B69C78", // Camel
+      "#78B69C", // Teal
+      "#9C78B6", // Periwinkle
+      "#B69C78", // Tan
+      "#789CB6", // Powder blue
+      "#B6789C", // Dusty pink
+      "#9CB678", // Lime
+      "#B9847A", // Terracotta
+      "#7AB986", // Mint
+      "#A37DB0", // Lilac
+    ];
+    const randomIndex = Math.floor(Math.random() * colors.length);
+
+    return colors[randomIndex];
+  };
+
+  const photoRef = useRef<HTMLDivElement | null>(null);
+  const colorRef = useRef<string | null>(null);
+
+  useEffect(() => {
+    if (!colorRef.current) {
+      colorRef.current = getRandomColor();
+    }
+    if (photoRef.current && colorRef.current && playerRank > 3) {
+      const color = colorRef.current;
+      const photoStyle = photoRef.current.style;
+      photoStyle.backgroundImage = `linear-gradient(45deg, ${color}, transparent 45%, ${color})`;
+      photoStyle.borderColor = color;
+      photoStyle.color = color;
+    }
+  }),
+    [];
+
   return (
     <div
       className={`player clearfix rotateInDown${animationDirection} animated`}
     >
-      <div className="photo pull-left">
+      <div className="photo pull-left" ref={photoRef}>
         {teamName === "UnKnown Team" ? "?" : teamName.slice(0, 1)}
       </div>
       <div className="desc pull-left">
